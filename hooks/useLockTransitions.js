1c8b8bbaf8681b7b9b3eb6a67f336f0a3a3783e5
Jul1enF <j.furic@gmail.com>
2025-11-27 17:39:53 +0100

    added useLockTransitions

---
+++ hooks/useLockTransitions.js
@@ -0,0 +1,31 @@
+import { useEffect, useRef } from "react";
+
+export function useLockTransition(refs) {
+    const timeout = useRef(null);
+
+    const array = Array.isArray(refs) ? refs
+        : typeof refs === "object" && refs !== null ? Object.values(refs)
+        : [refs];
+
+    const nodesArray = array.map(e => e.current ? e.current : e)
+
+    useEffect(() => {
+        const freezeTransitions = () => {
+            nodesArray.forEach(node => node?.classList?.add("noTransition"));
+
+            // reset timer if the function is triggered again
+            clearTimeout(timeout.current);
+
+            timeout.current = setTimeout(() => {
+                nodesArray.forEach(node => node?.classList?.remove("noTransition"));
+            }, 10);
+        }
+
+        window.addEventListener("resize", freezeTransitions);
+
+        return () => {
+            window.removeEventListener("resize", freezeTransitions);
+            clearTimeout(timeout.current);
+        };
+    }, []);
+}
