9f715e19e14c6e126f86fbfb3140d423755a4494
Jul1enF <j.furic@gmail.com>
2025-12-13 09:23:23 +0100

    changed warning logic

--- components/ui/ConfirmationModal.js
+++ components/ui/ConfirmationModal.js
@@ -2,7 +2,7 @@ import styles from "@/styles/ui/ConfirmationModal.module.css"
 import useLockBodyScroll from "@/hooks/useLockBodyScroll";
 
 
-export default function ConfirmationModal({ visible, confirmationText, warningText, confirmationButtonText, cancelButtonText, confirmationFunction, closeModal }) {
+export default function ConfirmationModal({ visible, confirmationText, warning, confirmationButtonText, cancelButtonText, confirmationFunction, closeModal }) {
 
     // Stop the scroll of the body of the page when scrolling in the menu
     useLockBodyScroll(visible);
@@ -18,8 +18,8 @@ export default function ConfirmationModal({ visible, confirmationText, warningTe
 
                     <div className={`line ${styles.thisLine}`} />
 
-                    <p className="warning" style={!warningText ? { height: 0, marginTop: 0 } : {}}>
-                        {warningText}
+                    <p className={`warning ${warning.success ? "success" : "error"}`} style={!warning.text ? { height: 0, marginTop: 0 } : {}}>
+                        {warning.text}
                     </p>
 
                     <button type="button" className="regularItem brightRedBg regularText" onClick={confirmationFunction}>{confirmationButtonText}</button>
